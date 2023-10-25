4805a11343c6ed0d2cebdbba7aaed973ba375fb7
Kremon <kremoor@gmail.com>
2023-10-25 21:52:05 +0300

    .

--- lab1/good_practice/start_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker stop bad_app
-docker rm bad_app
-docker build -t bad_app --file Bad_dockerfile .
-docker run --name bad_app -d -p 1111:1111 bad_app
-docker exec bad_app python3 /app/app.py #&>/dev/null &
